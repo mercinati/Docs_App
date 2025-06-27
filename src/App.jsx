f2d1049d45ce872ec14dcd75ba68e40ae3d3cbb6
karan gupta <karangupta9755@gmail.com>
2025-06-27 17:19:31 +0530

    Created Background Component and Fonts & selection color added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Background from './components/Background'
 
 const App = () => {
   return (
     <> 
-    <h1>helliooo</h1>
+      <div className='relative w-full h-screen bg-zinc-800'>
+        <Background />
+        <div className='fixed top-0 left-0 z-[3] w-full h-full '></div>
+      </div>
     </>
   )
 }
