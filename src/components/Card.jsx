f6d67a3aead24ff829136295199ebe325c3b3c70
karan gupta <karangupta9755@gmail.com>
2025-06-29 16:02:39 +0530

    Data seeded and dynamic values added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { FaFileAlt } from "react-icons/fa";        //file icon
-import { MdOutlineFileDownload } from "react-icons/md";      //download icon
+import { FaFileAlt } from "react-icons/fa";   //file icon
+import { MdOutlineFileDownload } from "react-icons/md";   //download icon
+import { IoClose } from "react-icons/io5";   //close icon  
+// import { motion, scale } from "motion/react"
 
-const Card = () => {
+const Card = ({data}) => {
   return (
-    <>
-    <div className='m-2 relative w-60 h-65 p-5 rounded-[30px] bg-zinc-900/90 text-white overflow-hidden shadow-md shadow-black/50'>
+    
+    <div className='relative flex-shrink-0 w-55 h-62 p-5 rounded-[30px] bg-zinc-900/90 text-white overflow-hidden shadow-md shadow-black/50'>
         <FaFileAlt />
-        <p className='text-sm leading-tight mt-5 font-semibold '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, distinctio!</p>
+        <p className='text-sm leading-tight mt-5 font-semibold '>{data.desc}</p>
         <div className='footer absolute left-0 w-full bottom-0  '>
-            <div className='flex items-center justify-between py-3 px-7 mb-3'>
-                <h5>0.4mb</h5>
+            <div className='flex items-center justify-between py-1 px-7 mb-3'>
+                <h5>{data.filesize}</h5>
                 <span className='w-6 h-6 bg-zinc-500 rounded-full flex items-center justify-center text-zinc-900'>
-                    <MdOutlineFileDownload size=".9em"/>
+                    {data.close ? <IoClose /> : <MdOutlineFileDownload size=".9em"/>}   
                 </span>
             </div>
-            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center font-semibold'>
-                <h3 className='text-md'>Download Now</h3>
+            {data.tag.isOpen && (
+            <div className={`tag w-full py-4 bg-${data.tag.tagColor}-600 flex items-center justify-center font-semibold`}>
+                <h3 className='text-md'>{data.tag.tagTitle}</h3>  
             </div>
+            ) }
         </div>
     </div>
-    </>
+  
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+
+  // description , filesize , close or download icon , tag
+  const data= [
+    {
+      desc: "This is a sample file description to show how the card will look like in the foreground. ", 
+      filesize: "0.4mb", 
+      close: true, 
+      tag: {isOpen: true, tagTitle: "Download Now",tagColor: "green"}, 
+    },
+    {
+      desc: "This is a sample file description to show how the card will look like in the foreground. ", 
+      filesize: "0.4mb", 
+      close: false, 
+      tag: {isOpen: false, tagTitle: "Download Now",tagColor: "green"}, 
+    },
+    {
+      desc: "This is a sample file description to show how the card will look like in the foreground. ", 
+      filesize: "0.4mb", 
+      close: true, 
+      tag: {isOpen: true, tagTitle: "Download Now",tagColor: "sky"}, 
+    }
+  ];
+
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full '>
-        <Card />
+    <div className='fixed top-0 left-0 z-[3] w-full h-full p-3 flex gap-4 flex-wrap '>
+        {data.map((item, index)=>(
+          <Card data={item} />
+        ))}
     </div>
   )
 }
